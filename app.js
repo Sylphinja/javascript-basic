a14a9ef16b10fa0903bb130b9ef25aa0f69319b6
Sylphinja <113279770+Sylphinja@users.noreply.github.com>
2022-09-25 17:49:04 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+// console.log("Ict School");
+
+
+// var a = 5;// integer
+// var b ="hello";// string
+// var c = true || false;//boolean
+// var d = 5.3; // float
+
+// if (a == 5) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+
+// if (a === 5) {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+// Quiz
+//> 80 =  A
+//80-70= B
+//69-60=c
+//59-50=D
+// < 50=f
+
+var score = 50;
+
+
+if (score > 80) {
+    console.log("A");
+}else if (score >= 70){
+console.log("B");
+}else if (score >= 60){
+    console.log("C");
+}else if(score >= 50){
+    console.log("D");
+}else{
+    console.log("F");
+}
