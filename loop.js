e9ac04a2ebb5eb8c352241bbba49d17b813dbbd6
Sylphinja <113279770+Sylphinja@users.noreply.github.com>
2022-10-02 17:53:16 +0700

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,34 @@
+// for(let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// let i = 0;
+// while (i<= 5) {
+//     console.log(i);
+//     i++;
+
+
+// let lastNumber = 10;
+
+// let result=0;
+
+
+// for(let i =1; i <= lastNumber; i++) {
+//     result= result + i;
+// }
+// console.log(result);
+
+// let time = 2;
+
+
+// for (let i = 1; i <= 12; i++) {
+//     console.log(`${time} x ${i} =${time * i}`);
+// }
+
+
+for(let r= 1; r <=100; r++) {
+    if (r % 2 == 0) {
+    console.log(r);
+}
+}
+
